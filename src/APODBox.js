b81b8e7c8bb3b10d0f8cae0dbdfc67c20c2aa034
abe-one <77995299+abe-one@users.noreply.github.com>
2021-03-17 18:03:33 -0500

    Complete MMVP

--- src/APODBox.js
+++ src/APODBox.js
@@ -3,31 +3,28 @@ import axios from "axios";
 import CycleButton from "./CycleButton";
 
 /*
-
-
 img click enlarge
 <prev button>
 <rand button>
 <nxt button>
-split into components
-
 
 */
 
-export default function APODBox({ singleAPOD }) {
+export default function APODBox({ aPOD }) {
+  if (!aPOD) return <h3>Loading</h3>;
   return (
     <div className="container APOD-box">
       <div className="img-box">
-        {<img src={singleAPOD.url} alt={singleAPOD.title} />}{" "}
-        {singleAPOD.copyright && (
-          <p className="copyright">Copyright {singleAPOD.copyright}</p>
-        )}
-        <h2>{singleAPOD.title}</h2>
-        <h4>{singleAPOD.explanation}</h4>
-        {/* <CycleButton buttonText="This Yesterday" eventFunction={null} />
-        <CycleButton buttonText="Random Day" eventFunction={null} />
-        <CycleButton buttonText="This Tomorrow" eventFunction={null} /> */}
+        <img src={aPOD.url} alt={aPOD.title} />
       </div>
+      {aPOD.copyright && (
+        <p className="copyright">Copyright {aPOD.copyright}</p>
+      )}
+      <h2>{aPOD.title}</h2>
+      <h4>{aPOD.explanation}</h4>
+      {/* <CycleButton buttonText="This Yesterday" eventFunction={null} />
+      <CycleButton buttonText="Random Day" eventFunction={null} />
+      <CycleButton buttonText="This Tomorrow" eventFunction={null} />  */}
     </div>
   );
 }
--- src/App.js
+++ src/App.js
@@ -5,38 +5,33 @@ import "./App.css";
 import APODBox from "./APODBox";
 
 function App() {
-  const [aPODs, setAPODs] = useState([
-    {
-      copyright: "Donald Mitchell",
-      date: "2021-03-17",
-      explanation:
-        "If you could stand on Venus -- what would you see?  Pictured is the view from Venera 13, a robotic Soviet lander which parachuted and air-braked down through the thick Venusian atmosphere in March of 1982. The desolate landscape it saw included flat rocks, vast empty terrain, and a featureless sky above Phoebe Regio near Venus' equator. On the lower left is the spacecraft's penetrometer used to make scientific measurements, while the light piece on the right is part of an ejected lens-cap.  Enduring temperatures near 450 degrees Celsius and pressures 75 times that on Earth, the hardened Venera spacecraft lasted only about two hours. Although data from Venera 13 was beamed across the inner Solar System almost 40 years ago, digital processing and merging of Venera's unusual images continues even today.  Recent analyses of infrared measurements taken by ESA's orbiting Venus Express spacecraft indicate that active volcanoes may currently exist on Venus.",
-      hdurl: "https://apod.nasa.gov/apod/image/2103/Venus_Venera13_1705.jpg",
-      media_type: "image",
-      service_version: "v1",
-      title: "The Surface of Venus from Venera 13",
-      url: "https://apod.nasa.gov/apod/image/2103/Venus_Venera13_960.jpg",
-    },
-  ]);
+  const [aPOD, setAPOD] = useState();
+  // {
+  //   copyright: "Donald Mitchell",
+  //   date: "2021-03-17",
+  //   explanation:
+  //     "If you could stand on Venus -- what would you see?  Pictured is the view from Venera 13, a robotic Soviet lander which parachuted and air-braked down through the thick Venusian atmosphere in March of 1982. The desolate landscape it saw included flat rocks, vast empty terrain, and a featureless sky above Phoebe Regio near Venus' equator. On the lower left is the spacecraft's penetrometer used to make scientific measurements, while the light piece on the right is part of an ejected lens-cap.  Enduring temperatures near 450 degrees Celsius and pressures 75 times that on Earth, the hardened Venera spacecraft lasted only about two hours. Although data from Venera 13 was beamed across the inner Solar System almost 40 years ago, digital processing and merging of Venera's unusual images continues even today.  Recent analyses of infrared measurements taken by ESA's orbiting Venus Express spacecraft indicate that active volcanoes may currently exist on Venus.",
+  //   hdurl: "https://apod.nasa.gov/apod/image/2103/Venus_Venera13_1705.jpg",
+  //   media_type: "image",
+  //   service_version: "v1",
+  //   title: "The Surface of Venus from Venera 13",
+  //   url: "https://apod.nasa.gov/apod/image/2103/Venus_Venera13_960.jpg",
+  // },
 
-  // useEffect(() => {
-  //   axios
-  //     .get(`${BASE_URL}?api_key=DEMO_KEY`)
-  //     // .get(`${BASE_URL}?api_key=${API_KEY}&count=1`) // API with key
-  //     .then((res) => {
-  //       setAPODs(res.data);
-  //       // console.log(res);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}?api_key=DEMO_KEY`)
+      // .get(`${BASE_URL}?api_key=${API_KEY}`) // API with key
+      .then((res) => {
+        setAPOD(res.data);
+        console.log(res + "this is the orginal res");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  return (
-    <div className="App">
-      <APODBox singleAPOD={aPODs[0]} />
-    </div>
-  );
+  return <div className="App">{<APODBox aPOD={aPOD} />}</div>;
 }
 
 export default App;
