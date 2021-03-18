c75acea636e534abf36d5da99b5b548e1d8342bf
abe-one <77995299+abe-one@users.noreply.github.com>
2021-03-17 19:29:06 -0500

    in progress buttons

--- src/App.js
+++ src/App.js
@@ -5,29 +5,19 @@ import "./App.css";
 import APODBox from "./APODBox";
 
 function App() {
-  const [aPOD, setAPOD] = useState();
-  // {
-  //   copyright: "Donald Mitchell",
-  //   date: "2021-03-17",
-  //   explanation:
-  //     "If you could stand on Venus -- what would you see?  Pictured is the view from Venera 13, a robotic Soviet lander which parachuted and air-braked down through the thick Venusian atmosphere in March of 1982. The desolate landscape it saw included flat rocks, vast empty terrain, and a featureless sky above Phoebe Regio near Venus' equator. On the lower left is the spacecraft's penetrometer used to make scientific measurements, while the light piece on the right is part of an ejected lens-cap.  Enduring temperatures near 450 degrees Celsius and pressures 75 times that on Earth, the hardened Venera spacecraft lasted only about two hours. Although data from Venera 13 was beamed across the inner Solar System almost 40 years ago, digital processing and merging of Venera's unusual images continues even today.  Recent analyses of infrared measurements taken by ESA's orbiting Venus Express spacecraft indicate that active volcanoes may currently exist on Venus.",
-  //   hdurl: "https://apod.nasa.gov/apod/image/2103/Venus_Venera13_1705.jpg",
-  //   media_type: "image",
-  //   service_version: "v1",
-  //   title: "The Surface of Venus from Venera 13",
-  //   url: "https://apod.nasa.gov/apod/image/2103/Venus_Venera13_960.jpg",
-  // },
+  const [aPOD, setAPOD] = useState([]);
+  const [random, setRandom] = useState(false);
 
   useEffect(() => {
     axios
-      .get(`${BASE_URL}?api_key=DEMO_KEY`)
+      .get(`${BASE_URL}?api_key=DEMO_KEY&count=1`)
       // .get(`${BASE_URL}?api_key=${API_KEY}`) // API with key
       .then((res) => {
-        setAPOD(res.data);
-        console.log(res + "this is the orginal res");
+        setAPOD(res.data[0]);
+        console.log(res + "this is the original res");
       })
       .catch((err) => {
-        console.log(err);
+        alert(err);
       });
   }, []);
 
