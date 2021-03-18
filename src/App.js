a4365f5ba20db999ec798ef2d1bba430eb511b16
abe-one <77995299+abe-one@users.noreply.github.com>
2021-03-17 21:49:11 -0500

    Test/Prototype random button

--- src/App.js
+++ src/App.js
@@ -8,20 +8,31 @@ function App() {
   const [aPOD, setAPOD] = useState([]);
   const [random, setRandom] = useState(false);
 
+  const getRandomAPOD = () => {
+    setRandom(true);
+  };
+
   useEffect(() => {
     axios
-      .get(`${BASE_URL}?api_key=DEMO_KEY&count=1`)
+      .get(`${BASE_URL}?api_key=DEMO_KEY${random ? "&count=1" : ""}`)
       // .get(`${BASE_URL}?api_key=${API_KEY}`) // API with key
       .then((res) => {
-        setAPOD(res.data[0]);
+        // setAPOD(res.data[0]);
+        Array.isArray(res.data) ? setAPOD(res.data[0]) : setAPOD(res.data);
         console.log(res + "this is the original res");
       })
       .catch((err) => {
         alert(err);
-      });
-  }, []);
+      })
+      .finally();
+  }, [random]);
 
-  return <div className="App">{<APODBox aPOD={aPOD} />}</div>;
+  return (
+    <div className="App">
+      {<APODBox aPOD={aPOD} />}
+      <button onClick={getRandomAPOD}>Rando</button>
+    </div>
+  );
 }
 
 export default App;
